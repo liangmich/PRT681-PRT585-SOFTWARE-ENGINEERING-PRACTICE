f3f401a73fd8cce308839cf2b67f664857072e20
liangmich <liangmich98@gmail.com>
2026-07-26 18:13:43 +0930

    create MvcMovie project in week1HelloWorldMVC folder

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Models;
+using MvcMovie.Models;
 
-namespace WebApplication1.Controllers
+namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1.Models
+namespace MvcMovie.Models
 {
     public class ErrorViewModel
     {
