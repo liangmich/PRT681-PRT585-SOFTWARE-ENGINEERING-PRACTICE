63eb09e7a4993942abd63c6c326fcf088bd96ebe
liangmich <liangmich98@gmail.com>
2026-07-26 18:51:48 +0930

    Add views for HelloWorld controller

--- week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e3addd49880c72d42a6a6b41e0736ff1642422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82301d397cba9f15538b07968d5a1ac32effee5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
