6257196ee96dba2686bc7bc3cbb7992ab9f4ddd7
liangmich <liangmich98@gmail.com>
2026-07-30 16:46:18 +0930

    Add search by title and genre

--- week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cd84654d1122639f08fe811ad9fc1fa9717783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e623b54ecfb12e9f4bc913deff44c3dbfbef65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
