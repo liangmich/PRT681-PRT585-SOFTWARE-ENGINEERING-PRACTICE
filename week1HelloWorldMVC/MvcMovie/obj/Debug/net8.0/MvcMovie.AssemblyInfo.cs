c02103fcd52002da54a6025abf01717531e9958a
liangmich <liangmich98@gmail.com>
2026-07-30 17:03:34 +0930

    Add validation rules to Movie model

--- week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ week1HelloWorldMVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6257196ee96dba2686bc7bc3cbb7992ab9f4ddd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328f25e01367f963ec4ec469687ee0f145eec176")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
